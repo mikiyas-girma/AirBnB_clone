4fc2af40fef3c8079792ed21466fabb9d9e0b341
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-03-10 00:29:28 +0300

    documented the code and module

--- models/base_model.py
+++ models/base_model.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
+"""a module containing a base model for all objects"""
 
 from datetime import datetime
 import uuid
 
 
 class BaseModel:
-
+    """illustrates a base class """
     def __init__(self):
+        """initializes an instance of a BaseModel class"""
         self.created_at = datetime.now()
         self.updated_at = self.created_at
         self.id = uuid.uuid4().hex
 
     def save(self):
+        """updates the time an instance is last update"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """returns a dictionary consisting of all key and values
+            contained in BaseModel instance along with class name
+        """
         for key, value in self.__dict__.items():
             if isinstance(value, datetime):
                 self.__dict__.__setitem__(key, value.isoformat())
@@ -22,4 +28,5 @@ class BaseModel:
         return self.__dict__
 
     def __str__(self):
+        """returns a custom string representation of BaseModel instance"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
