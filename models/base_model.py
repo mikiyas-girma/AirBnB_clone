4e3347826d53e6fc64d313a1939758ef36bce1d6
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-03-09 22:56:23 +0300

    initialization and str print part done

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
 
-from datetime import datetime
+import datetime
 import uuid
 
 
 class BaseModel:
 
     def __init__(self):
-        self.id = uuid.uuid4().hex
-        self.created_at = datetime.datetime.now
+        self.created_at = datetime.datetime.now()
         self.updated_at = self.created_at
+        self.id = uuid.uuid4().hex
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def to_dict(self):
+        a = self.__dict__
+        return a
+
+
+b = BaseModel()
+print(b)
