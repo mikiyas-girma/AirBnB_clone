446eda2679e2d410fe2b5dc327eafffea27728b6
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-03-09 20:48:29 +0300

    used hex to get string of uuid4

--- models/base_model.py
+++ models/base_model.py
@@ -7,6 +7,6 @@ import uuid
 class BaseModel:
 
     def __init__(self):
-        self.id = str(uuid.uuid4())
+        self.id = uuid.uuid4().hex
         self.created_at = datetime.datetime.now
         self.updated_at = self.created_at
