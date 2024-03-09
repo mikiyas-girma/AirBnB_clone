f53caa7cd2f5d65198ba571f9250139eee00af31
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-03-10 00:19:10 +0300

    finished  task 3.BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
 
-import datetime
+from datetime import datetime
 import uuid
 
 
 class BaseModel:
 
     def __init__(self):
-        self.created_at = datetime.datetime.now()
+        self.created_at = datetime.now()
         self.updated_at = self.created_at
         self.id = uuid.uuid4().hex
 
-    def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    def save(self):
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        a = self.__dict__
-        return a
+        for key, value in self.__dict__.items():
+            if isinstance(value, datetime):
+                self.__dict__.__setitem__(key, value.isoformat())
+        self.__dict__.__setitem__('__class__', self.__class__.__name__)
+        return self.__dict__
 
-
-b = BaseModel()
-print(b)
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
