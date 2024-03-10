8edf3aeab1c3ba5a369b38bbae50817b469d4841
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-03-10 13:19:09 +0300

    done file storage engine

---
+++ models/engine/file_storage.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""the module contains a class used for serialization
+and deserialization process"""
+
+import json
+import os
+
+
+class FileStorage:
+    """FileStorage class containing methods for
+    creating, storing and restoring file data"""
+
+    __file_path = 'obj.json'
+    __objects = {}
+
+    def all(self):
+        """returns all the stored objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """stores a new object that is not stored previously"""
+        obj_key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[obj_key] = obj
+
+    def save(self):
+        """encodes objects and writes it to json file
+        """
+        with open(self.__file_path, 'w', encoding='utf-8') as f:
+            f.write(json.JSONEncoder().encode(self.__objects))
+
+    def reload(self):
+        """decodes from json file to object if the file exists
+        """
+        if os.path.isfile(self.__file_path) is True:
+            with open(self.__file_path, mode='r', encoding='utf-8') as f:
+                file_content = f.readlines()
+            if len(file_content) > 0:
+                file_txt = ''.join(file_content)
+            else:
+                file_txt = '{}'
+            self.__objects = json.JSONDecoder().decode(file_txt)
