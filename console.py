86c3aa515e59d52f757e8f9ed6887b8c5de8dd71
mikiyas girma <mikiyasgirmaet@gmail.com>
2024-03-11 04:20:49 +0300

    update the console so it works also for added classes

--- console.py
+++ console.py
@@ -5,9 +5,15 @@ import cmd
 import shlex
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 from models import storage
 
-class_list = {'BaseModel', 'FileStorage', 'User'}
+class_list = {'BaseModel', 'FileStorage', 'User', 'Amenity',
+              'Review', 'Place', 'City', 'State'}
 
 
 class HBNBCommand(cmd.Cmd):
