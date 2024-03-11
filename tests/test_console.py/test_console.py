063d08b07bb92a5d3238ffe1300b4e6a7b1c4b08
meliance <melakmu66@gmail.com>
2024-03-11 20:19:01 +0300

    adding test cases

--- tests/test_console.py/test_console.py
+++ tests/test_console.py/test_console.py
@@ -2,3 +2,43 @@
 """
 Unit tests for console using context manager for capturing stdout
 """
+
+import os
+import sys
+import unittest
+from io import StringIO
+from unittest.mock import patch
+from console import HBNBCommand
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+
+class TestConsole(unittest.TestCase):
+    """
+    Unittest for the console model
+    """
+
+    def setUp(self):
+        """Set up test environment"""
+        self.console = HBNBCommand()
+
+    def test_quit(self):
+        """Test quit command"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertTrue(self.console.onecmd("quit"))
+
+    def test_help(self):
+        """Test help command"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(self.console.onecmd("help"))
+            self.assertTrue(self.console.onecmd("help quit"))
+
+if __name__ == '__main__':
+    unittest.main()
+
